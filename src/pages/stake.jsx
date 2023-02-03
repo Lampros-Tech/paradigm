4b44ea9a938d441ca243144c1bda838b7252fe45
sarvagna <sarvagnakadiya1@gmail.com>
2023-02-04 01:34:24 +0530

    started integration

--- src/pages/stake.jsx
+++ src/pages/stake.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "../styles/stake.scss";
 import { useState } from "react";
+import { useAccount, useProvider, useSigner, useContract } from "wagmi";
+import { CONTRACT_ADDRESS } from "../config";
+import stakeFIL_abi from "../contract/stakeFIL.json";
 
 function Stake() {
+  //instance of connected contract
+  const connectedContract = useContract({
+    address: CONTRACT_ADDRESS,
+    abi: stakeFIL_abi,
+  });
+
+  //function to stake
+  const stakeFilCoin = async () => {
+    let stakeTx = await connectedContract.addUser(Date.now(), { value: 0.001 });
+    const receipt = await stakeTx.wait();
+    if (receipt) {
+      console.log("transaction successful");
+    }
+  };
   const [number, setNumber] = useState("");
 
   const handleNumber = (e) => {
@@ -16,8 +33,7 @@ function Stake() {
   };
   return (
     <div className="stake-main">
-      <div className="home-bg">
-      </div>
+      <div className="home-bg"></div>
       <div className="stake-form">
         <div className="stake-main">
           <h2 className="header">Stake your FIL & earn interest over it</h2>
@@ -33,7 +49,9 @@ function Stake() {
             />
           </div>
           <div>
-            <button className="stake-btn">Stake</button>
+            <button className="stake-btn" onClick={() => stakeFilCoin}>
+              Stake
+            </button>
           </div>
         </div>
       </div>
